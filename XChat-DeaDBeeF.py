4d3254d27499f7c2ba1a1c149f082e5364f5c333
iceTwy <nerorush23@gmail.com>
2013-05-26 18:34:14 +0200

    Added additional commands for current song

--- XChat-DeaDBeeF.py
+++ XChat-DeaDBeeF.py
@@ -72,15 +72,17 @@ def deadbeef_next_song(word, word_eol, userdata):
 	is_deadbeef_running()
 	os.system("deadbeef --next")
 	print "Next track loaded!"
-	read_song = os.popen('deadbeef --nowplaying "%a - %t - %b% (%y) // %@:BITRATE@kbps"').read().strip()
+	sleep(0.001) # DeaDBeeF shows the initial track if isn't given some time to update
+	read_song = os.popen('deadbeef --nowplaying "%t by %a - from %b (%y)"').read()
 	print "You are listening to: %s" % str(read_song)
 	return xchat.EAT_ALL
  
 def deadbeef_previous_song(word, word_eol, userdata):
 	is_deadbeef_running()
-	print "Previous track loaded!"
 	os.system("deadbeef --prev")
-	read_song = os.popen('deadbeef --nowplaying "%a - %t - %b% (%y) // %@:BITRATE@kbps"').read().strip()
+	print "Previous track loaded!"
+	sleep(0.001) # DeaDBeeF shows the initial track if isn't given some time to update 
+	read_song = os.popen('deadbeef --nowplaying "%t by %a - from %b% (%y)"').read()
 	print "You are listening to: %s" % str(read_song)
 	return xchat.EAT_ALL
  
@@ -106,12 +108,19 @@ if __name__ == '__main__':
 	
 	is_deadbeef_running()
 	
-	xchat.hook_command('currentsong',deadbeef_current_song) 
-	xchat.hook_command('nextsong',deadbeef_next_song)
-	xchat.hook_command('prevsong',deadbeef_previous_song)
+#Display the current track (chose one)
+	#xchat.hook_command('currentsong',deadbeef_current_song) 
+	#xchat.hook_command('nowplaying',deadbeef_current_song)
+	xchat.hook_command('np',deadbeef_current_song)
+
+#Play, pause, stop track
 	xchat.hook_command('playsong',deadbeef_play_song)
 	xchat.hook_command('pausesong',deadbeef_pause_song)
 	xchat.hook_command('stopsong',deadbeef_stop_song)
+	
+#Skip to next or previous song	
+	xchat.hook_command('nextsong',deadbeef_next_song)
+	xchat.hook_command('prevsong',deadbeef_previous_song)
 	
 	print "XChat-DeaDBeeF %s plugin loaded successfully! - by %s" % (__module_version__,__module_author__)
 	print "Protip: use /playsong to play a song or resume playback (if paused)."

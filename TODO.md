60c3e759a47f3cd0de89d9d456cbd157a1fa73ee
iceTwy <nerorush23@gmail.com>
2013-05-26 15:57:06 +0200

    Version 0.3. Checks implemented

--- TODO.md
+++ TODO.md
@@ -1,6 +1,5 @@
 ## TO-DO List
 
-- Check if DeaDBeeF is launched prior to executing any control command.
-- Look into the play/pause matter with some builds of DeaDBeeF.
+- Use subprocess to check if DeaDBeeF is running, instead of os.popen.
 - Put up a list of recommended keybinds for control commands.
 
--- XChat-DeaDBeeF.py
+++ XChat-DeaDBeeF.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 #
-#  XChat-DeaDBeeF.py  -  XChat plugin for DeaDBeeF integration
+#  XChat-DeaDBeeF  -  XChat plugin for DeaDBeeF integration
 #
 #
 #            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
@@ -22,7 +22,7 @@
 
 __module_name__ = "XChat-DeaDBeeF"
 __module_description__ = "DeaDBeeF integration in XChat."
-__module_version__ = "0.2"
+__module_version__ = "0.3"
 __module_author__ = "Trixar_za & iceTwy"
 __module_credits__ = ["Trixar_za", "iceTwy"]
 __module_license__ = "WTFPL"
@@ -33,36 +33,84 @@ __module_status__ = "Released"
 ###########################
 import xchat
 import os
+import subprocess
+from threading import Thread
+from time import sleep
 ###########################
 
+def call_deadbeef():
+	sleep(5)
+	print "DeadBeeF lauched!"
+	subprocess.call("deadbeef")
+
+def is_deadbeef_running():
+	runningornot = os.popen("ps -e | grep deadbeef-main").read()
+	if len(runningornot) == 0:
+		print "DeaDBeeF is not running. Launching in 5s..."
+		call_deadbeef_thread = Thread(target=call_deadbeef)
+		call_deadbeef_thread.start()
+	else:
+		pass
+		
+def is_song_loaded():
+	loadedornot = os.popen("deadbeef --nowplaying %e").read()
+	if "nothing" in loadedornot:
+		print "Track loaded!"
+	else:
+		print "Track resumed!"
+
 def deadbeef_current_song(word, word_eol, userdata):
-      read_song = os.popen('deadbeef --nowplaying "%a - %t - %b% (%y) // %@:BITRATE@kbps"').read().strip()
-      nickname = "♪ NP @ %s ♪ - " % xchat.get_info("nick")
-      announce_song = nickname + str(read_song)
-      xchat.command("say "+announce_song)
-      return xchat.EAT_ALL
+	is_deadbeef_running()
+	read_song = os.popen('deadbeef --nowplaying "%a - %t - %b% (%y) // %@:BITRATE@kbps"').read().strip()
+	nickname = "♪ NP @ %s ♪ - " % xchat.get_info("nick")
+	announce_song = nickname + str(read_song)
+	xchat.command("say "+announce_song)
+	return xchat.EAT_ALL
       
 def deadbeef_next_song(word, word_eol, userdata):
-    os.system("deadbeef --next")
-    return xchat.EAT_ALL
+	is_deadbeef_running()
+	os.system("deadbeef --next")
+	print "Next track loaded!"
+	read_song = os.popen('deadbeef --nowplaying "%a - %t - %b% (%y) // %@:BITRATE@kbps"').read().strip()
+	print "You are listening to: %s" % str(read_song)
+	return xchat.EAT_ALL
  
 def deadbeef_previous_song(word, word_eol, userdata):
-    os.system("deadbeef --prev")
-    return xchat.EAT_ALL
+	is_deadbeef_running()
+	print "Previous track loaded!"
+	os.system("deadbeef --prev")
+	read_song = os.popen('deadbeef --nowplaying "%a - %t - %b% (%y) // %@:BITRATE@kbps"').read().strip()
+	print "You are listening to: %s" % str(read_song)
+	return xchat.EAT_ALL
  
-def deadbeef_playpause_song(word, word_eol, userdata):
-    os.system("deadbeef --play-pause")
-    return xchat.EAT_ALL
+def deadbeef_play_song(word, word_eol, userdata):
+	is_deadbeef_running()
+	is_song_loaded()
+	os.system("deadbeef --play")
+	return xchat.EAT_ALL
  
+def deadbeef_pause_song(word, word_eol, userdata):
+	is_deadbeef_running()
+	os.system("deadbeef --pause")
+	print "Current track paused!"
+	return xchat.EAT_ALL
+	
 def deadbeef_stop_song(word, word_eol, userdata):
-    os.system("deadbeef --stop")
-    return xchat.EAT_ALL
-    
-xchat.hook_command('currentsong',deadbeef_current_song) 
-xchat.hook_command('nextsong',deadbeef_next_song)
-xchat.hook_command('prevsong',deadbeef_previous_song)
-xchat.hook_command('playpausesong',deadbeef_playpause_song)
-xchat.hook_command('stopsong',deadbeef_stop_song)
-
-print "XChat-DeaDBeeF %s plugin loaded successfully! - by %s" % (__module_version__,__module_author__)
-print "Protip: use /playpausesong to play, pause and resume playback (if paused)."
+	is_deadbeef_running()
+	os.system("deadbeef --stop")
+	print "Current track stopped!"
+	return xchat.EAT_ALL
+	
+if __name__ == '__main__':
+	
+	is_deadbeef_running()
+	
+	xchat.hook_command('currentsong',deadbeef_current_song) 
+	xchat.hook_command('nextsong',deadbeef_next_song)
+	xchat.hook_command('prevsong',deadbeef_previous_song)
+	xchat.hook_command('playsong',deadbeef_play_song)
+	xchat.hook_command('pausesong',deadbeef_pause_song)
+	xchat.hook_command('stopsong',deadbeef_stop_song)
+	
+	print "XChat-DeaDBeeF %s plugin loaded successfully! - by %s" % (__module_version__,__module_author__)
+	print "Protip: use /playsong to play a song or resume playback (if paused)."
